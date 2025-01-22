06a3fa214b799fef6b5d919fb11600db6c77984b
Ahmed Ryan <ahmedryanfaiyaz@gmail.com>
2025-01-22 11:59:41 +0600

    feature: step five completed;

--- src/main.py
+++ src/main.py
@@ -6,10 +6,13 @@ def parse_input():
     prompt = input('')  # Read user input
     words = prompt.split()
 
-    if len(words) < 3:
-        exit()
+    if len(words) == 2:
+        return words[0], '', words[1]
 
-    return words[0], words[1], words[2]
+    if len(words) == 3:
+        return words[0], words[1], words[2]
+
+    exit()
 
 
 def validate_input(tool_name, tool_function, file_name):
@@ -25,28 +28,32 @@ def get_path(file_name):
     return Path(f"{utils.get_project_root()}/data/{file_name}")
 
 
-def get_total_bytes(path, file_name):
+def get_total_bytes(path):
     bytes_in_file = path.read_bytes()
     byte_count = len(bytes_in_file)
-    return f"{byte_count} {file_name}"
+    return byte_count
 
 
-def get_total_lines(path, file_name):
+def get_total_lines(path):
     line_count = sum(1 for line in path.open(encoding='utf-8'))
-    return f"{line_count} {file_name}"
+    return line_count
 
 
-def get_total_words(path, file_name):
+def get_total_words(path):
     content = path.read_text(encoding='utf-8')
     words = content.split()
     word_count = len(words)
-    return f"{word_count} {file_name}"
+    return word_count
 
 
-def get_total_characters(path, file_name):
+def get_total_characters(path):
     content = path.read_text(encoding='utf-8')
     char_count = len(content) + content.count('\n') # adding content count probably to count \n as 2 characters instead of 1
-    return f"{char_count} {file_name}"
+    return char_count
+
+
+def print_result(counts, filename):
+    print(f'{' '.join(map(str, counts))} {filename}')
 
 
 def main():
@@ -57,13 +64,20 @@ def main():
 
         try:
             if tool_function == '-c':
-                print(get_total_bytes(path, file_name))
+                counts = [get_total_bytes(path)]
+                print_result(counts, file_name)
             elif tool_function == '-l':
-                print(get_total_lines(path, file_name))
+                counts = [get_total_lines(path)]
+                print_result(counts, file_name)
             elif tool_function == '-w':
-                print(get_total_words(path, file_name))
+                counts = [get_total_words(path)]
+                print_result(counts, file_name)
             elif tool_function == '-m':
-                print(get_total_characters(path, file_name))
+                counts = [get_total_characters(path)]
+                print_result(counts, file_name)
+            elif tool_function == '':
+                counts = [get_total_lines(path), get_total_words(path), get_total_bytes(path)]
+                print_result(counts, file_name)
 
         except FileNotFoundError:
             print("Error: The file does not exist.")
